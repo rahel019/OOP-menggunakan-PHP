2faffcc4c2465fa5db58c0ed599b03f6b77672b7
Rahelnapitupulu_019 <151107370+rahel019@users.noreply.github.com>
2024-04-12 21:18:51 +0700

    Create coba.php

---
+++ coba.php
@@ -0,0 +1,16 @@
+ <?php
+
+ class Coba{
+
+
+ }
+//object dari class
+ $a = new Coba();
+ $b = new Coba();
+ $c = new Coba();
+
+ //untuk meliahta isi dari object
+ var_dump($a);
+ var_dump($b);
+ var_dump($c);
+ 
